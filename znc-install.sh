5a4b4256f29fcecaa04c6ab28864d9ba8666ac8b
frankthetank7254 <frankthetank7254@gmail.com>
2015-03-22 01:26:31 -0400

    Spruce up informational text

--- znc-install.sh
+++ znc-install.sh
@@ -90,6 +90,8 @@ fi
 #### User Script Starts ####
 ############################
 #
+echo "This script will download and install znc on your slot. It may take a few minutes..."
+sleep 3
 mkdir -p ~/bin
 wget -qO ~/znc.tar.gz http://znc.in/releases/znc-latest.tar.gz
 tar xf ~/znc.tar.gz && cd ~/znc-1.*
@@ -97,7 +99,9 @@ tar xf ~/znc.tar.gz && cd ~/znc-1.*
 make && make install
 cd && rm -rf znc{-1.*,.tar.gz}
 ~/bin/znc --makeconf
-
+#
+echo -e "\033[33m""\nNow that ZNC has been installed, configured, and started, we will make sure it starts if/when your server reboots.""\e[0m"
+#
 # adding to cron
 tmpnum=$(shuf -i 10001-20000 -n 1) 
 if [ "$(crontab -l)" == "no crontab for $(whoami)" ]; then
@@ -116,12 +120,10 @@ else
                 echo "znc is already in crontab"
         fi
 fi
-
 # give user the full URL
-echo "Click on the URL below to do additional configuration if needed"
-echo -e "\nhttps://$(hostname -f):$(grep Port ~/.znc/configs/znc.conf | awk '{print $3}')"
-
-
+echo -e "\nClick on the URL below to do additional configuration if needed"
+echo -e "\033[33m""https://$(hostname -f):$(grep Port ~/.znc/configs/znc.conf | awk '{print $3}')""\e[0m"
+echo -e "\nSearch the ZNC wiki for help connecting with different clients. http://wiki.znc.in/"
 #
 #
 ############################
