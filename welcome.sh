fe4054073d60ed1f2df1203e4e5d0b056d1a6f57
frankthetank7254 <frankthetank7254@gmail.com>
2015-03-26 10:53:06 -0400

    add whole disk check

--- welcome.sh
+++ welcome.sh
@@ -2,3 +2,4 @@
 echo -e "\033[33m""Welcome $(whoami)""\e[0m"
 screen -ls
 echo -e "\033[33m""You have $(du -s --si ~/ | awk '{print $1;}') of free space""\e[0m"
+echo -e "\033[33m""The disk that you are on is $(df  -h $(df -h ~/ | grep dev | awk '{print $1}') | grep dev | awk '{print $5;}') full""\e[0m"
