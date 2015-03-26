c06440142e8febe9b57e57c77454670aa20e9cb0
frankthetank7254 <frankthetank7254@gmail.com>
2015-03-26 12:10:56 +0000

    change web dir for pastes

--- feralpaste.sh
+++ feralpaste.sh
@@ -2,5 +2,6 @@
 [ $# -ge 1 -a -f "$1" ] && input="$1" || input="-"
 mkdir -p ~/www/$(whoami).$(hostname)/public_html/pastes
 tmpfile=$(mktemp | awk -F'.' '{print $2;}')
-cp $input ~/www/$(whoami).$(hostname)/public_html/$tmpfile
-echo https://$(hostname -f)/$(whoami)/$tmpfile
+cp $input ~/www/$(whoami).$(hostname)/public_html/pastes/$tmpfile.txt
+echo https://$(hostname -f)/$(whoami)/pastes/$tmpfile.txt
+
