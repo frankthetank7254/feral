1425d4be6e5d8b317db74f09ec6269c8fb66c784
frankthetank7254 <frankthetank7254@gmail.com>
2015-03-27 12:02:51 -0400

    add xclip comment

--- feralpaste.sh
+++ feralpaste.sh
@@ -4,4 +4,6 @@ mkdir -p ~/www/$(whoami).$(hostname)/public_html/pastes
 tmpfile=$(mktemp | awk -F'.' '{print $2;}')
 cp $input ~/www/$(whoami).$(hostname)/public_html/pastes/$tmpfile.txt
 echo https://$(hostname -f)/$(whoami)/pastes/$tmpfile.txt
+#if xclip workes on the slot, and you started ssh with "-X", the following line would get the URL on your clipboard
+# echo https://$(hostname -f)/$(whoami)/pastes/$tmpfile.txt | xclip -i
 
