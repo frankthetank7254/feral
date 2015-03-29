de903e435d063079191b2a774b0d8dd36729c0c0
frankthetank7254 <frankthetank7254@gmail.com>
2015-03-29 16:23:15 -0400

    cleanup

--- feralpaste.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-[ $# -ge 1 -a -f "$1" ] && input="$1" || input="-"
-mkdir -p ~/www/$(whoami).$(hostname)/public_html/pastes
-tmpfile=$(mktemp | awk -F'.' '{print $2;}')
-cp $input ~/www/$(whoami).$(hostname)/public_html/pastes/$tmpfile.txt
-echo https://$(hostname -f)/$(whoami)/pastes/$tmpfile.txt
-#if xclip workes on the slot, and you started ssh with "-X", the following line would get the URL on your clipboard
-# echo https://$(hostname -f)/$(whoami)/pastes/$tmpfile.txt | xclip -i
-
