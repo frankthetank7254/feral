8d6253753c066f4e5c33e633d1077d6f52063257
frankthetank7254 <frankthetank7254@gmail.com>
2015-03-29 16:22:30 -0400

    fix wording

--- welcome.sh
+++ welcome.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo -e "\033[33m""Welcome $(whoami)""\e[0m"
 screen -ls
-echo -e "\033[33m""You have $(du -s --si ~/ | awk '{print $1;}') of free space""\e[0m"
-echo -e "\033[33m""The disk that you are on is $(df  -h $(df -h ~/ | grep dev | awk '{print $1}') | grep dev | awk '{print $5;}') full""\e[0m"
+echo -e "\033[33m""You have used $(du -s --si ~/ | awk '{print $1;}')""\e[0m"
+echo -e "\033[33m""Your shared hard drive is $(df  -h $(df -h ~/ | grep dev | awk '{print $1}') | grep dev | awk '{print $5;}') full""\e[0m"
